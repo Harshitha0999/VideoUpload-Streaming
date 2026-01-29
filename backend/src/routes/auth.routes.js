53336e61c17b2367c79f5212218c978b4eec4250
Ganta Harshitha <137334199+Harshitha0999@users.noreply.github.com>
2026-01-29 20:56:27 +0530

    Update auth.routes.js
    Removed commented-out code for user authentication routes.

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,15 +1,4 @@
-// const express = require("express");
-// const {
-//   registerUser,
-//   loginUser,
-// } = require("../controllers/auth.controller");
 
-// const router = express.Router();
-
-// router.post("/register", registerUser);
-// router.post("/login", loginUser);
-
-// module.exports = router;
 
 const express = require("express");
 const router = express.Router();
